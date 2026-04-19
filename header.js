ec866955b1c90838857a62394c1ce252cf577564
saramesiti <smesiti24@epoka.edu.al>
2026-04-19 12:57:42 +0200

    simple js for header

--- header.js
+++ header.js
@@ -1,1 +1,12 @@
-
+ $(document).ready(function() {
+    function showWelcomeAlert() {
+        alert("Welcome to Five 7evens Agency!");
+        }
+    function handleBookingClick() {
+        alert("Going to booking form");
+            window.location.href = 'bookingForm.html';
+        }
+        showWelcomeAlert();
+    $('#search-btn').on('click', handleBookingClick);
+                
+});
