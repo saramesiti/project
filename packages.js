d69312e7f64273064c8388f94a30611909b11a1f
Vesa Xhemali <vxhemali@epoka.edu.al>
2026-04-26 12:45:50 +0200

    added reference to new page

--- packages.js
+++ packages.js
@@ -1,26 +1,27 @@
  $(document).ready(function() {
-    function parisClick(){
-    alert("Quick Details:\nAges: All Ages\nDuration: 6 Hours\nLocation: Paris\nTour Type: Guided Tour");
-}
-$('#Pprice').on('click', parisClick);
-function baliClick(){
-    alert("Quick Details:\nAges: All Ages\nDuration: 5 Hours\nLocation: Bali\nTour Type: Self-Guided Tour");
-}
-$('#Bprice').on('click', baliClick);
-function NewClick(){
-    alert("Quick Details:\nAges: All Ages\nDuration: 7 Hours\nLocation: New York\nTour Type: Guided Tour");
-}
-$('#Nprice').on('click', NewClick);
-function TokyoClick(){
-    alert("Quick Details:\nAges: All Ages\nDuration: 6 Hours\nLocation: Tokyo\nTour Type: Self-Guided Tour");
-}
-$('#Tprice').on('click', TokyoClick);
-function romeClick(){
-    alert("Quick Details:\nAges: All Ages\nDuration: 5 Hours\nLocation: Rome\nTour Type: Guided Tour");
-}
-$('#Rprice').on('click', romeClick);
-function dubaiClick(){
-    alert("Quick Details:\nAges: All Ages\nDuration: 6 Hours\nLocation: Dubai\nTour Type: Self-Guided Tour");
-}
-$('#Dprice').on('click', dubaiClick);
+
+    $('#Pprice').on('click', function() {
+        window.location.href = "details.html#paris";
+    });
+
+    $('#Bprice').on('click', function() {
+        window.location.href = "details.html#bali";
+    });
+
+    $('#Nprice').on('click', function() {
+        window.location.href = "details.html#nyc";
+    });
+
+    $('#Tprice').on('click', function() {
+        window.location.href = "details.html#tokyo";
+    });
+
+    $('#Rprice').on('click', function() {
+        window.location.href = "details.html#rome";
+    });
+
+    $('#Dprice').on('click', function() {
+        window.location.href = "details.html#dubai";
+    });
+
 });
